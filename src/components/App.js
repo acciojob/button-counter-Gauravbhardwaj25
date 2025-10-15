f0ea9a3c876b0681c148633be7205d82a675827f
Gaurav Bhardwaj <gauravbhardwaj0025@gmail.com>
2025-10-15 11:31:30 +0530

    update in p tag

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const [count, setCount] = useState(0)
 
   return (
     <div>
-      <p>Button Clicked {count} times</p>
+      <p>Button clicked {count} times</p>
        <button onClick={ () => setCount(count+1)}>Click me</button>
     </div>
   )
